d36ff2926da0a7d5b50162f2a5c9d0c465e49396
Megan Lukes@gmail.com <meglukes@gmail.com>
2022-01-17 11:28:17 -0500

    snake class adjustment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 import random, time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,31 +8,19 @@ screen.bgcolor("purple")
 screen.title("Pink Python")
 screen.tracer(0)
 
-snake = []
+snake = Snake()
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("DeepPink")
-    new_segment.pu()
-    new_segment.goto(position)
-    snake.append(new_segment)
-
-
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
-    for seg in range(len(snake) - 1, 0, -1):
-        new_x = snake[seg - 1].xcor()
-        new_y = snake[seg-1].ycor()
-        snake[seg].goto(new_x, new_y)
-    snake[0].forward(20)
-
-
+    snake.move()
 
 
 screen.exitonclick()
