d04853c2a76271b0635166d6f76801b9b7eef0ec
Megan Lukes@gmail.com <meglukes@gmail.com>
2022-01-17 12:47:59 -0500

    create scoreboard class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 import random, time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,8 @@ screen.title("Pink Python")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,6 +25,9 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    if snake.snake[0].distance(food) < 15:
+        print("yummy")
+        food.refresh()
 
 screen.exitonclick()
 
