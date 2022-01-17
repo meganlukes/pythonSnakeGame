16146538d492d70c73b25cbd1f1888583e783bbf
Megan Lukes@gmail.com <meglukes@gmail.com>
2022-01-17 12:52:51 -0500

    create scoreboard, food, and snake classes

---
+++ snake.py
@@ -0,0 +1,38 @@
+from turtle import Screen, Turtle
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+move_distance= 20
+
+class Snake:
+    
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in starting_positions:
+            new_segment = Turtle("square")
+            new_segment.color("DeepPink")
+            new_segment.pu()
+            new_segment.goto(position)
+            self.snake.append(new_segment)
+    
+    def move(self):
+        for seg in range(len(self.snake) - 1, 0, -1):
+            new_x = self.snake[seg - 1].xcor()
+            new_y = self.snake[seg-1].ycor()
+            self.snake[seg].goto(new_x, new_y)
+        self.snake[0].forward(move_distance)
+
+    def up(self):
+        if self.snake[0].heading() != 270:
+            self.snake[0].setheading(90)
+    def down(self):
+        if self.snake[0].heading() != 90:
+            self.snake[0].setheading(270)
+    def left(self):
+        if self.snake[0].heading() != 0:
+            self.snake[0].setheading(180)
+    def right(self):
+        if self.snake[0].heading() != 180:
+            self.snake[0].setheading(0)
