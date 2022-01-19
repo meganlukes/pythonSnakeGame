5bdf790bb6711c452009bb77b075fba6c1bde153
Megan Lukes@gmail.com <meglukes@gmail.com>
2022-01-19 10:44:17 -0500

    adjusted scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -6,6 +6,7 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.color("white")
+        self.hideturtle()
         self.pu()
         self.goto(0, 260)
         self.update_scoreboard()
